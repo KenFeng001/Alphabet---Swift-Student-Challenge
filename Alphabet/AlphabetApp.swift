bfa8caddd1b74a559e0be2a8122d3fd0d37e37cc
Ken Feng <102650865+KenFeng001@users.noreply.github.com>
2025-02-03 15:56:49 +0000

    Initial Commit

---
+++ Alphabet/AlphabetApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlphabetApp.swift
+//  Alphabet
+//
+//  Created by Zile Feng on 03/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct AlphabetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlphabetTests/AlphabetTests.swift
@@ -0,0 +1,16 @@
+//
+//  AlphabetTests.swift
+//  AlphabetTests
+//
+//  Created by Zile Feng on 03/02/2025.
+//
+
+import Testing
+
+struct AlphabetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
